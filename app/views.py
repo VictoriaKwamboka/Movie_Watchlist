d81c2a06f37c2265d547920da0410d683ce1fe64
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-04-28 06:24:31 +0300

    Add upcoming and now showing movies

--- app/views.py
+++ app/views.py
@@ -12,9 +12,11 @@ def index():
     '''
     # message = 'Moringa school is cool! '
     popular_movies = get_movies('popular')
+    upcoming_movies = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     print(popular_movies)
     title = 'Home - Welcome to the best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
